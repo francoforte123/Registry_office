05c96109623973f9f1d636d9b60c8d1839410c52
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-01-18 15:48:24 +0100

    modify the code

--- src/main/java/Registry_office/Repository/UserRepository.java
+++ src/main/java/Registry_office/Repository/UserRepository.java
@@ -10,4 +10,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+
+    //aggiungere la query per la funzione "findByEmail"
+    //aggiungere la query per la funzione "findAll" che viene fornita dalla repository
+    //creare un modo per rendere il codice usufruibile da tutti quelli che runnano il tuo codice (ovvero modificare il file yml per compilare il programma senza andare a modificare i parametri d'accesso al db)
 }
